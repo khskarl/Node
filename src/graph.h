dcc545769ab86968c750d04193b47092414e7b1d
Eu <karllhenning64@gmail.com>
2016-05-27 15:54:32 -0300

    Refactored some code

---
+++ src/graph.h
@@ -0,0 +1,94 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+
+#define sizeofArray(t) (sizeof(t) / sizeof(t[0]))
+#define MAX_CONNECTION_COUNT 4
+
+enum SlotType 
+{
+	Color,
+	Vec3,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct SlotDesc
+{
+	const char* name;
+	SlotType type;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Connection
+{
+	ImVec2 pos;
+	SlotDesc desc;
+
+	inline Connection()
+	{
+		pos.x = pos.y = 0.0f;
+		input = 0;
+	}
+
+	float v3[3];
+
+	struct Connection* input;
+	std::vector<Connection*> output;
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Node types
+
+struct NodeType
+{
+	const char* name;
+	SlotDesc inputConnections[MAX_CONNECTION_COUNT];
+	SlotDesc outputConnections[MAX_CONNECTION_COUNT];
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static struct NodeType s_nodeTypes[] =
+{
+	{
+		"Blend",
+		// Input connections
+		{
+			{ "Input1", SlotType::Color },
+			{ "Input2", SlotType::Color },
+		},
+		// Output
+		{
+			{ "Out", SlotType::Color },
+		},
+	},
+
+	{
+		"Threshold",
+		// Input connections
+		{
+			{ "Input1", SlotType::Color },
+		},
+		// Output
+		{
+			{ "Out", SlotType::Color },
+		},
+	},
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct Node
+{
+	ImVec2 pos;
+	ImVec2 size;
+	int id;
+	const char* name;
+	std::vector<Connection*> inputConnections;
+	std::vector<Connection*> outputConnections;
+};
+
+#endif // GRAPH_H
--- src/graph_drawing_utils.h
+++ src/graph_drawing_utils.h
@@ -2,8 +2,13 @@
 #define GRAPH_DRAWING_UTILS_H
 
 #include <imgui.h>
+#include "graph.h"
+
+static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x+rhs.x, lhs.y+rhs.y); }
+static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x-rhs.x, lhs.y-rhs.y); }
+
 
 void DrawHermite(ImDrawList* drawList, ImVec2 p1, ImVec2 p2);
-// void DrawNode(ImDrawList* drawList, ImVec2 p1, ImVec2 p2);
+void DrawNode(ImDrawList* drawList, ImVec2 offset, Node* node, int& node_selected);
 
 #endif
